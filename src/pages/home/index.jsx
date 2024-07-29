aaf5d37957c748440a626f7393b1a110aac672df
cheeseburgertony <1821955020@qq.com>
2024-07-29 22:49:32 +0800

    整体App架构

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,24 +1,10 @@
-import request from '@/services/request'
-import React, { memo, useEffect, useState } from 'react'
+import React, { memo } from 'react'
 
 const Home = memo(() => {
-  const [highScore, setHighScore] = useState({})
-
-  useEffect(() => {
-    request.get('/home/highscore').then(res => {
-      console.log(res);
-      setHighScore(res)
-    })
-  }, [])
-
 
   return (
     <div>
-      <h1>{highScore.title}</h1>
-      <h4>{highScore.subtitle}</h4>
-      <ul>
-        {highScore.list?.map(item => <li key={item.id}>{item.name}</li>)}
-      </ul>
+      
     </div>
   )
 })
