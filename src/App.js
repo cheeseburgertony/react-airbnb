536d7d0dfd22697b2dddc7eb6740c35a8db668bd
cheeseburgertony <1821955020@qq.com>
2024-07-29 19:37:04 +0800

    CSS样式重置

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { memo } from 'react'
-import '@/assets/css/index.less'
 
 const App = memo(() => {
   return (
--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from '@/App';
 
+import 'normalize.css'
+import '@/assets/css/index.less'
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
