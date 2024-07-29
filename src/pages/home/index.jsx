296751d579773c0d4c5724151f753a2f41b4e3f5
cheeseburgertony <1821955020@qq.com>
2024-07-29 20:11:01 +0800

    网络请求axios的封装和测试

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,8 +1,25 @@
-import React, { memo } from 'react'
+import request from '@/services/request'
+import React, { memo, useEffect, useState } from 'react'
 
 const Home = memo(() => {
+  const [highScore, setHighScore] = useState({})
+
+  useEffect(() => {
+    request.get('/home/highscore').then(res => {
+      console.log(res);
+      setHighScore(res)
+    })
+  }, [])
+
+
   return (
-    <div>Home</div>
+    <div>
+      <h1>{highScore.title}</h1>
+      <h4>{highScore.subtitle}</h4>
+      <ul>
+        {highScore.list?.map(item => <li key={item.id}>{item.name}</li>)}
+      </ul>
+    </div>
   )
 })
 
