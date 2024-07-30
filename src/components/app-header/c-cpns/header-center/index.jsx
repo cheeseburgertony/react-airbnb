8e05958377df5726c73912b2f86c19575197296e
cheeseburgertony <1821955020@qq.com>
2024-07-30 20:41:01 +0800

    feat: AppHeader中间搜索框

--- src/components/app-header/c-cpns/header-center/index.jsx
+++ src/components/app-header/c-cpns/header-center/index.jsx
@@ -1,10 +1,18 @@
 import React, { memo } from 'react'
 import { CenterWrapper } from './style'
+import IconSearch from '@/assets/svg/icon_search'
 
 const HeaderCenter = memo(() => {
   return (
     <CenterWrapper>
-      HeaderCenter
+      <div className="search-bar">
+        <div className="text">
+          搜索房源和体验
+        </div>
+        <div className="icon">
+          <IconSearch />
+        </div>
+      </div>
     </CenterWrapper>
   )
 })
