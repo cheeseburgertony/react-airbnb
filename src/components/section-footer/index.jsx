39a541dc89e9f8b319b56e263647ee84a565d4f8
cheeseburgertony <1821955020@qq.com>
2024-08-01 00:10:55 +0800

    feat: 全部Entire页面跳转和简单布局

--- src/components/section-footer/index.jsx
+++ src/components/section-footer/index.jsx
@@ -2,6 +2,7 @@ import PropTypes from 'prop-types'
 import React, { memo } from 'react'
 import { FooterWrapper } from './style'
 import IconMoreArrow from '@/assets/svg/icon_more_arrow'
+import { useNavigate } from 'react-router-dom'
 
 const SectionFooter = memo((props) => {
   const { name } = props
@@ -10,9 +11,14 @@ const SectionFooter = memo((props) => {
     showName = `显示更多${name}房源`
   }
 
+  const navigate = useNavigate()
+  const moreClickHandle = () => {
+    navigate('/entire')
+  }
+
   return (
     <FooterWrapper color={name ? '#00848A' : '#000'}>
-      <div className="info">
+      <div className="info" onClick={moreClickHandle}>
         <span className='text'>{showName}</span>
         <IconMoreArrow />
       </div>
