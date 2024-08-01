8216e0cb8006b7c0648aa9bf93792055211540d6
cheeseburgertony <1821955020@qq.com>
2024-08-01 20:26:53 +0800

    feat: 全部页面-过滤条部分

---
+++ src/pages/entire/c-cpns/entire-pagination/index.jsx
@@ -0,0 +1,10 @@
+import React, { memo } from 'react'
+import { PageWrapper } from './style'
+
+const EntirePagination = memo(() => {
+  return (
+    <PageWrapper>EntirePagination</PageWrapper>
+  )
+})
+
+export default EntirePagination
--- src/pages/entire/index.jsx
+++ src/pages/entire/index.jsx
@@ -1,12 +1,15 @@
 import React, { memo } from 'react'
 import { EntireWrapper } from './style'
+import EntireFilter from './c-cpns/entire-filter'
+import EntireRooms from './c-cpns/entire-rooms'
+import EntirePagination from './c-cpns/entire-pagination'
 
 const Entire = memo(() => {
   return (
     <EntireWrapper>
-      <div className="filter">filter-section</div>
-      <div className="rooms">rooms-section</div>
-      <div className="pagination">pagination-section</div>
+      <EntireFilter />
+      <EntireRooms />
+      <EntirePagination />
     </EntireWrapper>
   )
 })
