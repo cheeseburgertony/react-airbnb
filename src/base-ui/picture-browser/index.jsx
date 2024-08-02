95957e8edd5ce91ca0482952b6ba84a65e5c8b11
cheeseburgertony <1821955020@qq.com>
2024-08-02 15:19:35 +0800

    feat: 详情页面-图片浏览器图片切换动画效果实现

--- src/base-ui/picture-browser/index.jsx
+++ src/base-ui/picture-browser/index.jsx
@@ -1,10 +1,15 @@
 import PropTypes from 'prop-types'
-import React, { memo, useEffect } from 'react'
+import React, { memo, useEffect, useState } from 'react'
 import { BrowserWrapper } from './style'
 import IconClose from '@/assets/svg/icon_close'
+import IconArrowLeft from '@/assets/svg/icon_arrow_left'
+import IconArrowRight from '@/assets/svg/icon_arrow_right'
+import { CSSTransition, SwitchTransition } from 'react-transition-group'
 
 const PictureBrowser = memo((props) => {
   const { pictureUrls, closeClick } = props
+  const [currentIndex, setCurrentIndex] = useState(0)
+  const [isNext, setIsNext] = useState(true)
 
   // 进入图片浏览器时不显示滚动条以及相关功能，退出时恢复
   useEffect(() => {
@@ -15,19 +20,49 @@ const PictureBrowser = memo((props) => {
     }
   }, [])
 
+  // 关闭按钮
   const closeClickHandle = () => {
     if (closeClick) closeClick()
   }
 
+  // 左右切换图片按钮
+  const controlCilckHandle = (isNext = true) => {
+    let newIndex = isNext ? currentIndex + 1 : currentIndex - 1
+    if (newIndex < 0) newIndex = pictureUrls.length - 1
+    if (newIndex > pictureUrls.length - 1) newIndex = 0
+    setCurrentIndex(newIndex)
+    setIsNext(isNext)
+  }
+
   return (
-    <BrowserWrapper>
+    <BrowserWrapper isNext={isNext}>
       <div className="top">
         <div className="close-btn" onClick={closeClickHandle}>
           <IconClose />
         </div>
       </div>
-      <div className="list">{pictureUrls.length}</div>
-      <div className="indicator"></div>
+      <div className="slider">
+        <div className="control">
+          <div className="btn left" onClick={e => controlCilckHandle(false)}>
+            <IconArrowLeft width="77" height="77" />
+          </div>
+          <div className="btn right" onClick={e => controlCilckHandle(true)}>
+            <IconArrowRight width="77" height="77" />
+          </div>
+        </div>
+        <div className="picture">
+          <SwitchTransition mode='in-out'>
+            <CSSTransition
+              key={pictureUrls[currentIndex]}
+              classNames='pic'
+              timeout={200}
+            >
+              <img src={pictureUrls[currentIndex]} alt="" />
+            </CSSTransition>
+          </SwitchTransition>
+        </div>
+      </div>
+      <div className="preview"></div>
     </BrowserWrapper>
   )
 })
--- src/base-ui/picture-browser/style.js
+++ src/base-ui/picture-browser/style.js
@@ -7,6 +7,8 @@ export const BrowserWrapper = styled.div`
   right: 0;
   bottom: 0;
   left: 0;
+  display: flex;
+  flex-direction: column;
   background-color: #333;
 
   .top {
@@ -19,5 +21,75 @@ export const BrowserWrapper = styled.div`
       right: 25px;
       cursor: pointer;
     }
+  }
+
+  .slider {
+    flex: 1;
+    position: relative;
+    display: flex;
+    justify-content: center;
+
+    .control {
+      position: absolute;
+      z-index: 1;
+      top: 0;
+      right: 0;
+      bottom: 0;
+      left: 0;
+      display: flex;
+      justify-content: space-between;
+      color: #fff;
+
+      .btn {
+        display: flex;
+        align-items: center;
+        height: 100%;
+        width: 83px;
+        cursor: pointer;
+      }
+    }
+
+    .picture {
+      position: relative;
+      width: 100%;
+      height: 100%;
+      max-width: 105vh;
+      overflow: hidden;
+
+      img {
+        position: absolute;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        height: 100%;
+        margin: 0 auto;
+        user-select: none;
+      }
+
+      .pic-enter {
+        transform: translateX(${props => props.isNext ? '100%' : '-100%'});
+        opacity: 0;
+      }
+
+      .pic-enter-active {
+        transform: translateX(0);
+        opacity: 1;
+        transition: all 0.2s ease;
+      }
+
+      .pic-exit {
+        opacity: 1;
+      }
+
+      .pic-exit-active {
+        opacity: 0;
+        transition: all 200ms ease;
+      }
+    }
+  }
+
+  .preview {
+    height: 100px;
   }
 `
